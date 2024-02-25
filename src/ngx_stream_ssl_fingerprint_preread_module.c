2130153a9b6317ac048881e6c47e8068ec972d18
Jun Ouyang <ouyangjun1999@gmail.com>
2024-02-25 19:24:29 +0800

    feat: stream module support

--- src/ngx_stream_ssl_fingerprint_preread_module.c
+++ src/ngx_stream_ssl_fingerprint_preread_module.c
@@ -14,7 +14,6 @@ static ngx_stream_module_t  ngx_stream_ssl_fingerprint_preread_module_ctx = {
     NULL                                      /* merge server configuration */
 };
 
-
 ngx_module_t  ngx_stream_ssl_fingerprint_preread_module = {
     NGX_MODULE_V1,
     &ngx_stream_ssl_fingerprint_preread_module_ctx,      /* module context */
@@ -30,9 +29,99 @@ ngx_module_t  ngx_stream_ssl_fingerprint_preread_module = {
     NGX_MODULE_V1_PADDING
 };
 
+static ngx_int_t
+ngx_stream_ssl_greased(ngx_stream_session_t *s,
+                 ngx_http_variable_value_t *v, uintptr_t data)
+{
+    if (s->connection == NULL)
+    {
+        return NGX_OK;
+    }
+
+    if (s->connection->ssl == NULL)
+    {
+        return NGX_OK;
+    }
+
+    if (ngx_ssl_ja3(s->connection) == NGX_DECLINED)
+    {
+        return NGX_ERROR;
+    }
+
+    v->len = 1;
+    v->data = (u_char*)(s->connection->ssl->fp_tls_greased ? "1" : "0");
+
+    v->valid = 1;
+    v->no_cacheable = 1;
+    v->not_found = 0;
+
+    return NGX_OK;
+}
+
+static ngx_int_t
+ngx_stream_ssl_fingerprint(ngx_stream_session_t *s,
+                 ngx_http_variable_value_t *v, uintptr_t data)
+{
+    if (s->connection == NULL)
+    {
+        return NGX_OK;
+    }
+
+    if (s->connection->ssl == NULL)
+    {
+        return NGX_OK;
+    }
+
+    if (ngx_ssl_ja3(s->connection) == NGX_DECLINED)
+    {
+        return NGX_ERROR;
+    }
+
+    v->data = s->connection->ssl->fp_ja3_str.data;
+    v->len = s->connection->ssl->fp_ja3_str.len;
+    v->valid = 1;
+    v->no_cacheable = 1;
+    v->not_found = 0;
+
+    return NGX_OK;
+}
+
+static ngx_stream_variable_t  ngx_stream_ssl_ja3_variables_list[] = {
+
+    {   ngx_string("stream_ssl_greased"),
+        NULL,
+        ngx_stream_ssl_greased,
+        0, 0, 0
+    },
+
+    {   ngx_string("stream_ssl_ja3"),
+        NULL,
+        ngx_stream_ssl_fingerprint,
+        0, 0, 0
+    },
+
+};
 
 static ngx_int_t
 ngx_stream_ssl_fingerprint_preread_init(ngx_conf_t *cf)
 {
+    ngx_stream_variable_t          *v;
+    size_t                        l = 0;
+    size_t                        vars_len;
+
+    vars_len = (sizeof(ngx_stream_ssl_ja3_variables_list) /
+            sizeof(ngx_stream_ssl_ja3_variables_list[0]));
+
+    /* Register variables */
+    for (l = 0; l < vars_len ; ++l) {
+        v = ngx_stream_add_variable(cf,
+                &ngx_stream_ssl_ja3_variables_list[l].name,
+                ngx_stream_ssl_ja3_variables_list[l].flags);
+        if (v == NULL) {
+            continue;
+        }
+        *v = ngx_stream_ssl_ja3_variables_list[l];
+    }
+
     return NGX_OK;
 }
